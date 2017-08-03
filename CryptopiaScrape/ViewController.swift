1a2921b77a5703f03d852c34ce1eb2643b8047cc
Daniel Ziabko <danielziabko@Daniels-Mac.local>
2017-08-03 17:19:17 -0400

    Initial Commit

---
+++ CryptopiaScrape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptopiaScrape
+//
+//  Created by Daniel Ziabko on 2017-08-03.
+//  Copyright © 2017 Daniel Ziabko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
